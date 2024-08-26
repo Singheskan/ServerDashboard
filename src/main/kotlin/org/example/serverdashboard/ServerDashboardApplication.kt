8b8320f50dad32ea47e6b15b507086d1b8d314d0
drifterduck <drifterduck@yepmail.net>
2024-08-26 17:21:21 +0200

    Simple Thymeleaf GUI which shows which websites are online and offline

--- src/main/kotlin/org/example/serverdashboard/ServerDashboardApplication.kt
+++ src/main/kotlin/org/example/serverdashboard/ServerDashboardApplication.kt
@@ -22,6 +22,22 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.http.client.SimpleClientHttpRequestFactory
 
+// GUI
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class DashboardController(private val serverDashboardService: ServerDashboardService) {
+
+	@GetMapping("/")
+	fun showDashboard(model: Model): String {
+		val urlStatuses = serverDashboardService.getUrlStatuses()
+		model.addAttribute("urlStatuses", urlStatuses)
+		return "dashboard"
+	}
+}
+
 @Configuration
 class AppConfig {
 
@@ -47,6 +63,8 @@ class ServerDashboardService(
 	@Value("\${app.website.urls}")
 	private lateinit var websiteUrls: List<String>
 
+	private val urlStatuses: MutableMap<String, Boolean> = mutableMapOf()
+
 	@Scheduled(fixedDelayString = "\${app.check.interval}")
 	fun checkWebsites() {
 		println("Checking websites...")
@@ -55,23 +73,34 @@ class ServerDashboardService(
 				println("Checking URL: $url")
 				val response = restTemplate.getForEntity(URI(url), String::class.java)
 				println("Response from $url: ${response.statusCode}")
-				if (!response.statusCode.is2xxSuccessful) {
+				if (response.statusCode.is2xxSuccessful) {
+					urlStatuses[url] = true
+				} else {
+					urlStatuses[url] = false
 					sendFailureNotification(url, response.statusCodeValue)
 				}
 			} catch (ex: HttpClientErrorException) {
 				println("Client error when checking $url: ${ex.statusCode.value()}")
+				urlStatuses[url] = false
 				sendFailureNotification(url, ex.statusCode.value())
 			} catch (ex: HttpServerErrorException) {
 				println("Server error when checking $url: ${ex.statusCode.value()}")
+				urlStatuses[url] = false
 				sendFailureNotification(url, ex.statusCode.value())
 			} catch (ex: ResourceAccessException) {
 				println("Timeout or connection issue when checking $url: ${ex.message}")
+				urlStatuses[url] = false
 				sendFailureNotification(url, "Timeout or Connection issue")
 			} catch (ex: Exception) {
 				println("Exception when checking $url: ${ex.message}")
+				urlStatuses[url] = false
 				sendFailureNotification(url, null)
 			}
 		}
+	}
+
+	fun getUrlStatuses(): Map<String, Boolean> {
+		return urlStatuses
 	}
 
 	private fun sendFailureNotification(url: String, statusCode: Any?) {
@@ -82,6 +111,7 @@ class ServerDashboardService(
 		mailSender.send(message)
 	}
 }
+
 
 
 @SpringBootApplication
